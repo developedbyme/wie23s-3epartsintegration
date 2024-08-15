8c3fb45278d56364f58e159b7a6b6941754a4077
Mattias Ekendahl <mattias@developedbyme.com>
2024-08-15 15:21:03 +0200

    Update: connecting to database

--- api/index.js
+++ api/index.js
@@ -1,19 +1,42 @@
 // Import the Express module
+const path = require('path');
 const express = require('express');
+const mysql = require('mysql2/promise');
 
-// Create an instance of Express
-const app = express();
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// Define a route handler for the root URL ('/')
-app.get('/', (req, res) => {
-  res.json([
-    {"id": 1, "name": "Mattias"},
-    {"id": 2, "name": "Linus"}
-  ]);
-});
+let run = async function () {
+  const pool = await mysql.createPool({
+    host: 'mysql-28cd133a-wie23s-3epart.l.aivencloud.com',
+    port: 21549,
+    user: 'avnadmin',
+    password: process.env.DB_PASSWORD,
+    database: 'defaultdb',
+  });
 
-// Start the server on port 3000
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  //await pool.query('CREATE TABLE test (id int NOT NULL, name varchar(255), PRIMARY KEY (id))');
+  const [rows, fields] = await pool.query('SELECT * FROM test');
+  console.log(rows, fields);
+  
+  // Create an instance of Express
+  const app = express();
+  
+  // Define a route handler for the root URL ('/')
+  app.get('/', (req, res) => {
+    let getData = async function () {
+    const [rows, fields] = await pool.query('SELECT * FROM test');
+    res.send(JSON.stringify(rows) + " " +  JSON.stringify(fields));
+    }
+    getData();
+  });
+  
+  
+  // Start the server on port 3000
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+run();
